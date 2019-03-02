81fb66affbbd77cb8f09744c9842f5605d8d0a28
AliHewiagh <ali@icycle-global.com>
2019-03-02 21:38:54 +0700

    uploade alsahel shop to github

---
+++ app/Http/Controllers/Auth/AdminLoginController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Auth;
+
+class AdminLoginController extends Controller
+{
+
+    public function __construct() {
+        $this->middleware('guest:admin', ['except' => ['logout']]);
+    }
+
+   public function showLoginForm() {
+        return view('auth.admin-login');
+    }
+
+   public function login(Request $request) {
+
+    // validate form
+       $this->validate($request, [
+        'email' => 'required|email',
+        'password' => 'required|min:8'
+       ]);
+
+       $credentials = [
+           'email' => $request->email,
+           'password' => $request->password,
+       ];
+
+    // attempt to log the user in
+    if (Auth::guard('admin')->attempt($credentials, $request->remember)) {
+        return redirect()->intended(route('admin.dashboard'));
+    }
+
+    return redirect()->back()->withInput($request->only('email', 'remember'));
+
+   }
+
+
+   public function logout()
+    {
+        Auth::guard('admin')->logout();
+        return redirect('/admin/login');
+    }
+}
---
+++ resources/views/admin.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Admin Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+                    <p>Admin</p>
+                    {{-- <form id="logout-form" action="{{ route('admin/logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form> --}}
+
+                   {{-- @component('')
+
+                   @endcomponent --}}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
